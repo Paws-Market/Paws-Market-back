5e60c95b7cbcc63d4f8f42271a33ae81fe4729e5
baekdoeun <donna8780@naver.com>
2025-03-18 15:44:47 +0900

    feat:Order and OrderDetail Entity

---
+++ src/main/java/com/korit/pawsmarket/domain/OrderDetail/entity/OrderDetail.java
@@ -0,0 +1,57 @@
+package com.korit.pawsmarket.domain.OrderDetail.entity;
+
+import com.korit.pawsmarket.domain.OrderDetail.enums.ShippingStatus;
+import com.korit.pawsmarket.domain.order.entity.Order;
+import com.korit.pawsmarket.domain.product.entity.Product;
+import com.korit.pawsmarket.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "order_detail")
+public class OrderDetail extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_detail_id")
+    private Long orderDetailId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn( name = "order_id", nullable = false)
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product; // 원가(상품 가격)
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    @Column(name = "price")
+    private int price; //원가 (상품 가격)
+
+    @Column( name = "discount_rate", nullable = false)
+    private int discountRate; // 할인율 (주문 당시 할인율을 저장)
+
+    @Column(name = "final_price", nullable = false)
+    private int finalPrice; //할인 갸격 (price - (price * discountRate / 100))
+
+    @Column(name = "total_price", nullable = false)
+    private int totalPrice; //최종 가격 (finalPrice * quantity)
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "shipping_status", nullable = false)
+    private ShippingStatus shippingStatus; // 배송 상태(준비중, 배송중, 배송완료)
+/*
+    @Column(name = "is_refundable", nullable = false)
+    private boolean isRefundable; //환불 여부
+*/
+
+}
---
+++ src/main/java/com/korit/pawsmarket/domain/OrderDetail/enums/ShippingStatus.java
@@ -0,0 +1,8 @@
+package com.korit.pawsmarket.domain.OrderDetail.enums;
+
+public enum ShippingStatus {
+    READY, //배송 준비 중
+    SHIPPED, //배송 중
+    DELIVERED, //배송 완료
+    CANCELED, //주무 취소 됨
+}
